fd34e7392cc7fd28ab363130295956dfce7744ad
unknown <azamat.mnv@gmail.com>
2025-04-07 16:20:23 -0700

    Final commit: Customers Orders ETL pipeline.

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-etl_pipeline_csv
+# Customer Orders ETL Pipeline
+
+This project is an end-to-end ETL (Extract, Transform, Load) pipeline that processes customer order data from a CSV file and loads it into an SQLite database.
+
+## Features
+- ✅ Extract data from CSV
+- ✅ Transform and clean data (add `total_price`, format dates)
+- ✅ Load data into SQLite database
+- ✅ Verify data with sample SQL query
+
+## Technologies Used
+- Python
+- Pandas
+- SQLAlchemy
+- SQLite
+
+## How to Run
+
+1. Install dependencies:
+   pip install -r requirements.txt
+2. Run the ETL pipeline:
+   python .\etl_pipeline.py
+
+
+## Database
+
+The final data is stored in `customer_orders.db` in the table `orders`.
+
+## Output
+
+Sample output:
+✅ Data extraction successful!
+customer_id  order_id  order_date product  amount
+0            1      1001  2024-04-01   Apple      10
+1            2      1002  2024-04-01  Banana      20
+2            1      1003  2024-04-02  Orange      15
+3            3      1004  2024-04-03   Apple       5
+✅ Data transformation successful!
+customer_id  order_id order_date product  amount  total_price
+0            1      1001 2024-04-01   Apple      10           50
+1            2      1002 2024-04-01  Banana      20          100
+2            1      1003 2024-04-02  Orange      15           75
+3            3      1004 2024-04-03   Apple       5           25
+✅ Data loaded successfully into customer_orders.db!
+✅ Sample data from the database:
+(1, 1001, '2024-04-01 00:00:00.000000', 'Apple', 10, 50)
+(2, 1002, '2024-04-01 00:00:00.000000', 'Banana', 20, 100)
+(1, 1003, '2024-04-02 00:00:00.000000', 'Orange', 15, 75)
+(3, 1004, '2024-04-03 00:00:00.000000', 'Apple', 5, 25)
+
+## Author
+
+Azamat Muminov
+
--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+
 
 # Extract the data from .csv file
 def extract_dat(file_path):
@@ -11,7 +12,63 @@ def extract_dat(file_path):
         print(f'❌ Error in extracting data: {e}')
         return None
 
+
+# Transform the data in the csv file
+def transform_data(df):
+    try:
+        # Convert order_date to datetime
+        df['order_date'] = pd.to_datetime(df['order_date'])
+
+        #  Add a new column 'total_price'
+        price_per_item = 5
+        df['total_price'] = df['amount'] * price_per_item
+
+        #  Clean any missing values (if any)
+        df = df.dropna()
+
+        print(f'✅ Data transformation successful!')
+        return df
+    except Exception as e:
+        print(f'❌ Error in transforming data: {e}')
+        return None
+
+
+# Load the data into a Database
+def load_data(df, database_name='customer_orders.db'):
+    try:
+        # Create SQLAlchemy engine to SQLite
+        engine = create_engine(f'sqlite:///{database_name}')
+
+        # Write data to a table called 'orders'
+        df.to_sql('Orders', con=engine, if_exists='replace', index=False)
+
+        print(f"✅ Data loaded successfully into {database_name}!")
+    except Exception as e:
+        print(f"❌ Error in loading data: {e}")
+
+
+# Verify the data load
+def query_data(database_name='customer_orders.db'):
+    try:
+        engine = create_engine(f'sqlite:///{database_name}')
+        with engine.connect() as connection:
+            result = connection.execute(text("SELECT * FROM orders LIMIT 5;"))
+            print(f'✅ Sample data from the database:')
+            for row in result:
+                print(row)
+    except Exception as e:
+        print(f'❌ Error querying data: {e}')
+
+
 if __name__ == "__main__":
     file_path = 'data/source_data.csv'
     data = extract_dat(file_path)
     print(data.head())
+
+    if data is not None:
+        transform_data = transform_data(data)
+        print(transform_data.head())
+
+        if transform_data is not None:
+            load_data(transform_data)
+            query_data()
